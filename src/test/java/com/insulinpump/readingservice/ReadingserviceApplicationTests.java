e327020036bf67fc12db120f161480fcc35342d6
Rafa-Gamero <rafagameroarrabal@gmail.com>
2025-05-22 13:04:01 +0200

    commit inicial

---
+++ src/test/java/com/insulinpump/readingservice/ReadingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insulinpump.readingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
